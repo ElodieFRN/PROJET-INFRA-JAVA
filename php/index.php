2c15c67dedeba7012e1a158e6218b57e438798a0
EmyVi <54404811+EmyVi@users.noreply.github.com>
2019-12-24 13:42:36 +0100

    Add files via upload

--- php/index.php
+++ php/index.php
@@ -1,20 +1,22 @@
 <html>
 	<head>
 		<meta charset="utf-8"/>
+		<link rel="stylesheet" href="stylePagePrincipale.css" />
 		<title> Sport manager event </title>
 	</head>
 	<body>
+		<img src="banniere.png">
 		<form method="post" action="retourIndex.php">
   			<p>
-  				<label for="login">Identifiant :</label>
-  				<input type="text" name="login" placeholder="ex : Emy" />
+  				<label for="login">Identifiant :</label><br/>
+  				<input type="text" name="login" placeholder="ex : mail@gmail.com"/>
+				<br/><br/>
+  				<label for="pass">Mot de passe :</label><br/>
+				<input type="password" name="pass" />
   			</p>
-  			<p>
-  				<label for="pass">Mot de passe :</label>
-  				<input type="password" name="pass" />
-  				</p>
-  			<button type="submit">Se connecter</button>
-			<a href="inscription.php" ><input type="button" value="S'inscrire" </a><br/>
+  			<button type="submit">Se connecter</button><br/><br/>
+			<p> Pas de compte ? 
+			<a href="inscription.php" ><input type="button" value="Rejoins-nous !" </a></p>
 		</form>
 	</body>
 </html>
--- php/inscription.php
+++ php/inscription.php
@@ -4,47 +4,39 @@
 		<title> Sport manager event</title>
 	</head>
 	<body>
-		<?php
-		
-		// rechercher une valeur:
-		session_start();
-		$numeroUtilisateur = $_SESSION['utilisateur'];
-
-			echo '
-			<form method="post" action="retourInscription.php">
-			<fieldset>
-       				<legend>Vos coordonnées</legend>
+		<img src="banniere.png">
+		<form method="post" action="retourInscription.php">
+		<fieldset>
+       			<legend>Vos coordonnées</legend>
 			
-  				<label for="nom">Nom* :</label>
-  				<input type="text" name="nom" placeholder="ex : VIGNON" required autofocus /><br/>
-  				<br/>
+  			<label for="nom">Nom* :</label>
+  			<input type="text" name="nom" placeholder="ex : VIGNON" required autofocus /><br/>
+  			<br/>
   			
-  				<label for="prenom">Prénom* :</label>
-  				<input type="text" name="prenom" placeholder="ex : Emelyne" required /><br/>
-  				<br/>
+  			<label for="prenom">Prénom* :</label>
+  			<input type="text" name="prenom" placeholder="ex : Emelyne" required /><br/>
+  			<br/>
 
-  				<label for="phone">Téléphone :</label>
-  				<input type="tel" name="phone" /><br/>
+  			<label for="phone">Téléphone :</label>
+  			<input type="tel" name="phone" /><br/>
   				
        
-  			</fieldset><br/>
-  			<fieldset>
-       				<legend>Vos identifiants</legend>	
+  		</fieldset><br/>
+  		<fieldset>
+       			<legend>Vos identifiants</legend>	
   				
-				<label for="mail">E-mail* :</label>
-  				<input type="email" name="mail" placeholder="ex : mail@gmail.com" /><br/>
-  				<br/>
+			<label for="mail">E-mail* :</label>
+  			<input type="email" name="mail" placeholder="ex : mail@gmail.com" /><br/>
+  			<br/>
 
-  				<label for="pass">Mot de passe* :</label>
-  				<input type="password" name="pass"  required  /><br/>
-  			</fieldset>
+  			<label for="pass">Mot de passe* :</label>
+  			<input type="password" name="pass"  required  /><br/>
+  		</fieldset>
   			
-  			<p> * : informations obligatoires </p>
+  		<p> * : informations obligatoires </p>
   			
-  			<button type="submit">Envoyer</button><br/>
-  			<a href="index.php" ><input type="button" value="Annuler" </a><br/>
-			</form>
-			';
-		?>
+  		<button type="submit">Envoyer</button>
+  		<a href="index.php" ><input type="button" value="Annuler" </a><br/>
+		</form>
 	</body>
 </html>
